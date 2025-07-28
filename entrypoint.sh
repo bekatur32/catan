6f5fad49571d3644c2b7db0035bdf7180a77fe18
bekatur32 <103990593+bekatur32@users.noreply.github.com>
2025-07-28 14:02:59 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,11 @@ python manage.py migrate --noinput
 echo "Собираем статику..."
 python manage.py collectstatic --noinput
 
+# Создание суперпользователя
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" \
+| python manage.py shell
+
 exec "$@"
